275d402498de38d8587476ea9665b1275c11a440
백대홍 <bdh3620@naver.com>
2022-10-25 02:46:31 +0900

    Separating component filess

---
+++ src/components/Content.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+class Content extends Component {
+    render() {
+      return (
+        <article>
+        <h2>{this.props.title}</h2>
+          {this.props.sub}
+  
+        HTML is HyperText Markup Language    
+        </article>
+      );
+    }
+}
+  
+export default Content;
