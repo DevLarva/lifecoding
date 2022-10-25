aca11dad121ef5a9a050e7cbf6c16029f02e2b0b
백대홍 <bdh3620@naver.com>
2022-10-26 04:09:12 +0900

    constructor

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import Content from './components/Content';
 import Subject from './components/Subject';
 
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject : {title: 'WEB', sub:'World Wide Web',}
+     }
+  }
+
+    render() {
     return (
       <div className='App'>
-          <Subject title="WEB" sub="World wide web!"></Subject>
+          <Subject title= {this.state.subject.title} sub= {this.state.subject.sub}></Subject>
           <TOC></TOC>
           <Content title="HTML" sub=" HTML is HyperText Markup Language"></Content>
-          <Content title="qwe" sub="asdasdasdadse"></Content>
       </div>
       
     );
