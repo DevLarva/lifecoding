25f3e773e2e9f0a8e289a3898577046d056224ae
백대홍 <bdh3620@naver.com>
2022-10-31 01:45:12 +0900

    nothing

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -11,6 +11,7 @@ class TOC extends Component {
         <li key = {data[i].id}>
           <a
            href={"/content" + data[i].id}
+           data-id={data[i].id}
            onClick = {function(e) {
               e.preventDefault();
               this.props.onChangePage();
